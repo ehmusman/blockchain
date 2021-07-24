06b8c60b0eceb64717ff0b5a6b6244c8f1dcb8a1
ehmusman <ehmusman@gmail.com>
2021-07-24 15:42:50 +0500

    hashBloack method is created

--- 00-blockchain/dev/blockchain.js
+++ 00-blockchain/dev/blockchain.js
@@ -1,13 +1,14 @@
+const sha256 = require("sha256")
 function Blockchain() {
     this.chain = []
-    this.newTransactions = []
+    this.pendingTransactions = []
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         // nonce actually comes from a proof of work, in our case nonce is simply just a number
         hash: hash,
@@ -15,7 +16,7 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
         previousBlockHash: previousBlockHash
         // this is similar to the hash. but this is a hashed string of previous block. and hash is a compressed string of currnt block
     }
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock)
 
     return newBlock;
@@ -23,5 +24,24 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
 
 Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1]
+}
+
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    }
+    this.pendingTransactions.push(newTransaction)
+
+    return this.getLastBlock()['index'] + 1
+
+}
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData)
+
+    const hash = sha256(dataAsString)
+
+    return hash
 }
 module.exports = Blockchain
--- 00-blockchain/dev/test.js
+++ 00-blockchain/dev/test.js
@@ -1,9 +1,16 @@
 const Blockchain = require("./blockchain")
 
 const bitcoin = new Blockchain()
-bitcoin.createNewBlock(2389, "SF09ASDF90SADF9", "ASDF978ASD89F7")
-bitcoin.createNewBlock(289, "SF0wtrSDF90SADF9", "ASDF978ASDtwer7")
-bitcoin.createNewBlock(239, "SFwrtASDF90SADF9", "ASDF978ASD8wr7")
+bitcoin.createNewBlock(2389, "SF09ASDF90SADF9", "ASDF978ASD89F7s")
 
-console.log(bitcoin)
+bitcoin.createNewTransaction(11, "FAROOQER2344", 'USMAN6A9DF6')
+
+bitcoin.createNewBlock(289, "SF0wtrSDF90SADF9", "ASDF978ASDtwer7")
+
+bitcoin.createNewTransaction(12, "FAROOQER2344", 'USMAN6A9DF6')
+bitcoin.createNewTransaction(10, "FAROOQER2344", 'USMAN6A9DF6')
+bitcoin.createNewTransaction(15, "FAROOQER2344", 'USMAN6A9DF6')
+
+bitcoin.createNewBlock(239, "SFwrtASDF90SADF9", "ASDF978ASD8wr7d")
+console.log(bitcoin.chain[2])
 console.log(bitcoin.getLastBlock())
