197e522c20b9867d428136220ae0efd701590eed
ehmusman <ehmusman@gmail.com>
2021-07-25 22:27:05 +0500

    nonce incremented

--- 00-blockchain/dev/blockchain.js
+++ 00-blockchain/dev/blockchain.js
@@ -44,4 +44,27 @@ Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, 
 
     return hash
 }
+// what is the proof of work?
+// a proof of work is very important to blockchain technology.
+// and that is the reason the bitcoin or any other blockchains are very secure.
+// but what is proof of work?
+// every blockchain is pretty much a list of block. every single block has to be created and added to the chain. 
+// we want to make sure that every block that is added to the chain is legitimate. and has the correct transactions and correct data inside of it.
+// every time when we create a new block we first have to make sure that its is a legitmate block by mining it through proof of work. 
+// so what is a proof of work method actually do?
+// the proof of work method takes in a current block data and previous hash. and through this data that is supplied it is going to generate a specific hash. this specific has in our case is started with four zeros
+//  0000ZSDF77VXXZC087VZ0SDF785SDD6547F763F7SADF
+// how can we generate it? it will be done by trry and try, by guessing and checking. so we'll generate the hash many many times untill a lucky one time comes. and generate a hash that has four zeros at the beginig. 
+// we'll generate different possible hashes by using the same current data and previousBlock hash with different nonce number. 
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+    while (hash.substring(0, 5) !== "00000") {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+        console.log(hash)
+    }
+    return nonce
+    // our nonce is actually proof of work
+}
 module.exports = Blockchain
--- 00-blockchain/dev/test.js
+++ 00-blockchain/dev/test.js
@@ -1,16 +1,41 @@
 const Blockchain = require("./blockchain")
 
 const bitcoin = new Blockchain()
-bitcoin.createNewBlock(2389, "SF09ASDF90SADF9", "ASDF978ASD89F7s")
+// bitcoin.createNewBlock(2389, "SF09ASDF90SADF9", "ASDF978ASD89F7s")
 
-bitcoin.createNewTransaction(11, "FAROOQER2344", 'USMAN6A9DF6')
+// bitcoin.createNewTransaction(11, "FAROOQER2344", 'USMAN6A9DF6')
 
-bitcoin.createNewBlock(289, "SF0wtrSDF90SADF9", "ASDF978ASDtwer7")
+// bitcoin.createNewBlock(289, "SF0wtrSDF90SADF9", "ASDF978ASDtwer7")
 
-bitcoin.createNewTransaction(12, "FAROOQER2344", 'USMAN6A9DF6')
-bitcoin.createNewTransaction(10, "FAROOQER2344", 'USMAN6A9DF6')
-bitcoin.createNewTransaction(15, "FAROOQER2344", 'USMAN6A9DF6')
+// bitcoin.createNewTransaction(12, "FAROOQER2344", 'USMAN6A9DF6')
+// bitcoin.createNewTransaction(10, "FAROOQER2344", 'USMAN6A9DF6')
+// bitcoin.createNewTransaction(15, "FAROOQER2344", 'USMAN6A9DF6')
 
-bitcoin.createNewBlock(239, "SFwrtASDF90SADF9", "ASDF978ASD8wr7d")
-console.log(bitcoin.chain[2])
-console.log(bitcoin.getLastBlock())
+// bitcoin.createNewBlock(239, "SFwrtASDF90SADF9", "ASDF978ASD8wr7d")
+// console.log(bitcoin.chain[2])
+// console.log(bitcoin.getLastBlock())
+const previousBlockHash = "F856AD78F5768A5SDF75S"
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: "7D8F5DA6F587DAF",
+        recipient: "9768DF096AD06DFADF9"
+    },
+    {
+        amount: 11,
+        sender: "7Z8SDF689AD6SF8ZXC",
+        recipient: "876ASd68998ASDFADF9"
+    },
+    {
+        amount: 12,
+        sender: "63446ASDF765ASDF67",
+        recipient: "ASDF8796ASD9F876SADF69"
+    }
+]
+
+// const nonce = 100;
+
+const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData)
+console.log(nonce)
+const hash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
+console.log(hash)
